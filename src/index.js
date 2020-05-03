39d6b2ea2c768e987220a5e80b1138a851f02c77
刘思鑫 <fordreamxkhl@163.com>
2020-05-03 22:32:52 +0800

    chore: render方法

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
-import PostList from "./PostList";
+// import PostList from "./PostList";
+import Render from "./render";
 
-ReactDOM.render(<PostList />, document.getElementById("root"));
+ReactDOM.render(<Render />, document.getElementById("root"));
 
 // ReactDOM.render(
 //   <React.StrictMode>
